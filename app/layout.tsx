47fdeef074e3fbdbfbdbb71abb189587fd70cea9
Praveen Juge <hello@praveenjuge.com>
2025-08-10 22:41:11 +0530

    Update global styles and layout configuration
    - Changed primary color in globals.css for improved visual consistency.
    - Enhanced layout.tsx by adding a theme configuration for ClerkProvider, ensuring a cohesive appearance across authentication components.
    
    These updates refine the overall UI and enhance the user experience during authentication.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased text-sm">
+      <body className="text-sm antialiased caret-primary accent-primary selection:bg-primary selection:text-primary-foreground [font-feature-settings:&quot;ss02&quot;,_&quot;ss03&quot;,_&quot;ss04&quot;,_&quot;ss07&quot;,_&quot;ss08&quot;,_&quot;ss09&quot;] [text-rendering:optimizeLegibility] [touch-action:manipulation]">
         <ClerkProvider
           dynamic
           signInUrl="/login"
           signUpUrl="/register"
+          appearance={{
+            theme: "simple",
+          }}
         >
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </ClerkProvider>
