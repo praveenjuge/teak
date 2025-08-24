d15bfeceed0d16d909b35fc4f35e89a765046853
Praveen Juge <hello@praveenjuge.com>
2025-08-24 13:45:09 +0530

    refactor: remove redundant comment about individual layout customization for clarity

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -4,7 +4,6 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 /**
  * Shared layout configurations
  *
- * you can customise layouts individually from:
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
