48bcd7cbcec5f400c15a8f4d398f9c95216d91f4
Praveen Juge <hello@praveenjuge.com>
2025-08-25 00:54:05 +0530

    refactor: update ESLint configuration and middleware runtime settings for improved consistency

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -9,6 +9,7 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
+  runtime: 'nodejs',
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
