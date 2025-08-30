45fbf361e2d1c5059c119a2da5781c53fd845aec
Praveen Juge <hello@praveenjuge.com>
2025-08-30 22:24:07 +0530

    feat: initialize Teak extension with basic structure and configuration

---
+++ apps/extension-new/entrypoints/popup/main.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./style.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/chrome-extension";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const EXTENSION_URL = chrome.runtime.getURL(".");
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error(
+    "Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file"
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+    >
+      <header className="w-full">
+        <SignedOut>
+          <SignInButton mode="modal" />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
