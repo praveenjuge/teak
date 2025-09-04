5ddf7c148ebfa4f238b350b6c8d732509c8996eb
Praveen Juge <hello@praveenjuge.com>
2025-09-04 16:07:25 +0530

    feat: update color scheme in BottomPattern and TopPattern components; change fill colors to red and gray for improved contrast

--- apps/web/components/patterns/TopPattern.tsx
+++ apps/web/components/patterns/TopPattern.tsx
@@ -1,121 +1,121 @@
 export function TopPattern() {
   return (
-  <svg
-    aria-hidden="true"
-    className="pointer-events-none absolute top-0 left-0 z-[-1] hidden select-none md:block"
-    fill="none"
-    height="335"
-    viewBox="0 0 542 335"
-    width="542"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g clipPath="url(#clip0_4755_1448)">
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M333.407 -223.331L299.243 -95.8287L426.745 -61.6646L460.909 -189.167L333.407 -223.331Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M171.46 -129.831L137.296 -2.3287L263.894 31.5932L298.963 -95.6668L171.46 -129.831Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M9.51373 -36.3309L-24.6504 91.1713L101.948 125.093L137.016 -2.16681L9.51373 -36.3309Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M-152.433 57.1691L-186.597 184.671L-59.0949 218.835L-24.9308 91.3332L-152.433 57.1691Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M-314.38 150.669L-348.544 278.171L-221.042 312.335L-186.878 184.833L-314.38 150.669Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M-476.327 244.169L-510.491 371.671L-382.988 405.835L-348.824 278.333L-476.327 244.169Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M-638.273 337.669L-672.437 465.171L-544.935 499.335L-510.771 371.833L-638.273 337.669Z"
-      />
-      <path
-        className="fill-orange-300 dark:fill-neutral-700"
-        d="M-800.22 431.169L-834.384 558.671L-706.882 592.835L-672.718 465.333L-800.22 431.169Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M460.286 -189.571L426.122 -62.0687L553.624 -27.9046L587.788 -155.407L460.286 -189.571Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M298.339 -96.0709L263.894 31.5932L392.285 63.3253L425.841 -61.9068L298.339 -96.0709Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M136.392 -2.57094L101.948 125.093L229.73 159.095L263.894 31.5932L136.392 -2.57094Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-25.5545 90.929L-59.7186 218.431L68.3919 250.325L101.948 125.093L-25.5545 90.929Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-187.501 184.429L-221.665 311.931L-94.1631 346.095L-59.999 218.593L-187.501 184.429Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-349.448 277.929L-383.612 405.431L-256.11 439.595L-221.946 312.093L-349.448 277.929Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-511.395 371.429L-545.559 498.931L-418.057 533.095L-383.893 405.593L-511.395 371.429Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-673.342 464.929L-707.506 592.431L-580.003 626.595L-545.839 499.093L-673.342 464.929Z"
-      />
-      <path
-        className="fill-orange-200 dark:fill-neutral-800"
-        d="M-835.288 558.429L-869.452 685.931L-741.95 720.095L-707.786 592.593L-835.288 558.429Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M426.45 -64.177L392.285 63.3253C462.703 82.1936 535.083 40.4047 553.952 -30.0128L426.45 -64.177Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M263.894 31.5932L229.73 159.095C300.148 177.964 373.417 133.743 392.285 63.3253L263.894 31.5932Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M101.948 125.093L68.3919 250.325C138.809 269.194 210.862 229.513 229.73 159.095L101.948 125.093Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M-59.3907 216.323L-93.5548 343.825C-23.1373 362.694 49.5236 320.743 68.3919 250.325L-59.3907 216.323Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M-221.337 309.823L-255.502 437.325C-185.084 456.194 -112.704 414.405 -93.8353 343.987L-221.337 309.823Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M-383.284 403.323L-417.448 530.825C-347.031 549.694 -274.65 507.905 -255.782 437.487L-383.284 403.323Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M-545.231 496.823L-579.395 624.325C-508.978 643.194 -436.597 601.405 -417.729 530.987L-545.231 496.823Z"
-      />
-      <path
-        className="fill-orange-100 dark:fill-neutral-900"
-        d="M-707.178 590.323L-741.342 717.825C-670.924 736.694 -598.544 694.905 -579.676 624.487L-707.178 590.323Z"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_4755_1448">
-        <rect fill="white" height="335" width="542" />
-      </clipPath>
-    </defs>
-  </svg>
+    <svg
+      aria-hidden="true"
+      className="pointer-events-none absolute top-0 left-0 z-[-1] hidden select-none md:block"
+      fill="none"
+      height="335"
+      viewBox="0 0 542 335"
+      width="542"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_4755_1448)">
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M333.407 -223.331L299.243 -95.8287L426.745 -61.6646L460.909 -189.167L333.407 -223.331Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M171.46 -129.831L137.296 -2.3287L263.894 31.5932L298.963 -95.6668L171.46 -129.831Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M9.51373 -36.3309L-24.6504 91.1713L101.948 125.093L137.016 -2.16681L9.51373 -36.3309Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M-152.433 57.1691L-186.597 184.671L-59.0949 218.835L-24.9308 91.3332L-152.433 57.1691Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M-314.38 150.669L-348.544 278.171L-221.042 312.335L-186.878 184.833L-314.38 150.669Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M-476.327 244.169L-510.491 371.671L-382.988 405.835L-348.824 278.333L-476.327 244.169Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M-638.273 337.669L-672.437 465.171L-544.935 499.335L-510.771 371.833L-638.273 337.669Z"
+        />
+        <path
+          className="fill-red-300 dark:fill-gray-700"
+          d="M-800.22 431.169L-834.384 558.671L-706.882 592.835L-672.718 465.333L-800.22 431.169Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M460.286 -189.571L426.122 -62.0687L553.624 -27.9046L587.788 -155.407L460.286 -189.571Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M298.339 -96.0709L263.894 31.5932L392.285 63.3253L425.841 -61.9068L298.339 -96.0709Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M136.392 -2.57094L101.948 125.093L229.73 159.095L263.894 31.5932L136.392 -2.57094Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-25.5545 90.929L-59.7186 218.431L68.3919 250.325L101.948 125.093L-25.5545 90.929Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-187.501 184.429L-221.665 311.931L-94.1631 346.095L-59.999 218.593L-187.501 184.429Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-349.448 277.929L-383.612 405.431L-256.11 439.595L-221.946 312.093L-349.448 277.929Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-511.395 371.429L-545.559 498.931L-418.057 533.095L-383.893 405.593L-511.395 371.429Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-673.342 464.929L-707.506 592.431L-580.003 626.595L-545.839 499.093L-673.342 464.929Z"
+        />
+        <path
+          className="fill-red-200 dark:fill-gray-800"
+          d="M-835.288 558.429L-869.452 685.931L-741.95 720.095L-707.786 592.593L-835.288 558.429Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M426.45 -64.177L392.285 63.3253C462.703 82.1936 535.083 40.4047 553.952 -30.0128L426.45 -64.177Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M263.894 31.5932L229.73 159.095C300.148 177.964 373.417 133.743 392.285 63.3253L263.894 31.5932Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M101.948 125.093L68.3919 250.325C138.809 269.194 210.862 229.513 229.73 159.095L101.948 125.093Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M-59.3907 216.323L-93.5548 343.825C-23.1373 362.694 49.5236 320.743 68.3919 250.325L-59.3907 216.323Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M-221.337 309.823L-255.502 437.325C-185.084 456.194 -112.704 414.405 -93.8353 343.987L-221.337 309.823Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M-383.284 403.323L-417.448 530.825C-347.031 549.694 -274.65 507.905 -255.782 437.487L-383.284 403.323Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M-545.231 496.823L-579.395 624.325C-508.978 643.194 -436.597 601.405 -417.729 530.987L-545.231 496.823Z"
+        />
+        <path
+          className="fill-red-100 dark:fill-gray-900"
+          d="M-707.178 590.323L-741.342 717.825C-670.924 736.694 -598.544 694.905 -579.676 624.487L-707.178 590.323Z"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4755_1448">
+          <rect fill="white" height="335" width="542" />
+        </clipPath>
+      </defs>
+    </svg>
   );
 }
