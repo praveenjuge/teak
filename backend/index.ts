9cdd44371fec1c56b2adca18bc9a9f8cf681fa43
Praveen Juge <hello@praveenjuge.com>
2025-11-24 14:14:41 +0530

    fix: remove user management exports from index file

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,6 @@ export { api, internal } from "./convex/_generated/api";
 export type { Doc, Id } from "./convex/_generated/dataModel";
 export * from "./shared";
 
-// User management
-export * from "./convex/users";
-
 // Cards domain surface
 export * from "./convex/tasks/cards/cardLimit";
 export * from "./convex/tasks/cards/createCard";
