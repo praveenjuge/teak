ef933fb0716084d95fa275d5dd8c6c6367c417a4
Praveen Juge <hello@praveenjuge.com>
2025-11-26 10:13:21 +0530

    feat: integrate Sentry for error tracking and monitoring in client and server

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,73 @@
-import type { NextConfig } from 'next'
+import { withSentryConfig } from "@sentry/nextjs";
+import type { NextConfig } from "next";
 
 const securityHeaders: { key: string; value: string }[] = [
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
   },
   {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload'
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
   },
   {
-    key: 'X-Frame-Options',
-    value: 'DENY'
+    key: "X-Frame-Options",
+    value: "DENY",
   },
   {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
+    key: "X-Content-Type-Options",
+    value: "nosniff",
   },
   {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin'
-  }
-]
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+];
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
-    turbopackFileSystemCacheForDev: true
+    turbopackFileSystemCacheForDev: true,
   },
   async headers() {
     return [
       {
-        source: '/:path*',
-        headers: securityHeaders
-      }
-    ]
-  }
-}
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "teak-z6",
+
+  project: "teak-prod-web",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
