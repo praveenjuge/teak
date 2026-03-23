e4714169c9c0cdf21b682239ba809448f32bf9c5
Praveen Juge <hello@praveenjuge.com>
2026-03-23 18:12:08 +0530

    native macos app try out

---
+++ apps/mac/teak/teakApp.swift
@@ -0,0 +1,17 @@
+//
+//  teakApp.swift
+//  teak
+//
+//  Created by Praveen Juge on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct teakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
